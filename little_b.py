786144cbf1e1b47b5ab27ee52ee2d97269240b80
brunskins <62477802+brunskins@users.noreply.github.com>
2022-03-23 15:34:25 +0000

    update to the browser to add support for multiple tabs and to allow for searching without a url

--- little_b.py
+++ little_b.py
@@ -2,6 +2,12 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtWebEngineWidgets import *
 import sys
+global current_tab
+global tab_count
+global tab_urls
+current_tab=0
+tab_count=0
+tab_urls=["http://duckduckgo.com"]
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -37,20 +43,96 @@ class MainWindow(QMainWindow):
         self.url_bar.returnPressed.connect(self.navigate_to_url)
         navbar.addWidget(self.url_bar)
 
+        self.search_bar = QLineEdit()
+        self.search_bar.returnPressed.connect(self.search_for)
+        self.search_bar.resize(10 , 10)
+        navbar.addWidget(self.search_bar)
+
+        
+        #tab bar
+        tabbar = QToolBar()
+        self.addToolBar(tabbar)
+
+        new_tab_btn = QAction("new tab", self)
+        new_tab_btn.triggered.connect(self.new_tab)
+        tabbar.addAction(new_tab_btn)
+
+        next_tab_btn = QAction("next tab", self)
+        next_tab_btn.triggered.connect(self.next_tab)
+        tabbar.addAction(next_tab_btn)
+
+        previous_tab_btn = QAction("previous tab", self)
+        previous_tab_btn.triggered.connect(self.previous_tab)
+        tabbar.addAction(previous_tab_btn)
+
         self.browser.urlChanged.connect(self.update_url)
+
+    def new_tab(self, url):
+        global current_tab
+        global tab_count
+        global tab_urls
+        print("new tab")
+        tab_count=tab_count+1
+        tab_urls.append("http://duckduckgo.com")
+        current_url = self.browser.url()
+        print(current_url)
+        tab_urls[current_tab] = current_url.toString()
+        print(tab_urls)
+        current_tab=tab_count
+        self.browser.setUrl(QUrl(tab_urls[tab_count]))
+
+    def next_tab(self, url):
+        global current_tab
+        global tab_count
+        global tab_urls
+        print("alive")
+        current_url = self.browser.url()
+        tab_urls[current_tab] = current_url.toString()
+        print("next tab")
+        current_tab=current_tab+1
+        if current_tab > tab_count:
+            current_tab=0
+        print("current tab is ", current_tab)
+        print("total tabs is ", tab_count)
+        print(tab_urls)
+        self.browser.setUrl(QUrl(tab_urls[current_tab]))
+
+    def previous_tab(self, url):
+        global current_tab
+        global tab_count
+        global tab_urls
+        print("alive")
+        current_url = self.browser.url()
+        tab_urls[current_tab] = current_url.toString()   
+        print("previous tab")
+        current_tab=current_tab-1
+        if current_tab < 0:
+            current_tab=tab_count
+        print("current tab is ", current_tab)
+        print("total tabs is ", tab_count)
+        self.browser.setUrl(QUrl(tab_urls[current_tab]))
+
+    def search_for(self):
+        url = self.search_bar.text()
+        url2= 'https://duckduckgo.com/?q=' + url + '&t=h_&ia=web'
+        self.browser.setUrl(QUrl(url2))
     
-    def navigate_home(self):
+    def navigate_home(self, url):
         self.browser.setUrl(QUrl("http://duckduckgo.com"))
 
-    def update_url(self, url):
+    def update_url(self):
+        url= self.browser.url()
         self.url_bar.setText(url.toString())
 
     def navigate_to_url(self):
+        print("alive")
         url = self.url_bar.text()
         if "https://" not in url:
             if "http://" not in url:
                 url = "https://" + url
         self.browser.setUrl(QUrl(url))
+        print("alive 2")
+    
 app = QApplication(sys.argv)
 QApplication.setApplicationName("little b")
 window = MainWindow()
